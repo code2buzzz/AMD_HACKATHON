f24e8a4674dee3a9fe7f9e54dcf17422d45d4b09
Rameez Ahmad <rameezahmad20131a4245@gmail.com>
2026-06-16 14:06:28 +0530

    added code UBUNTU

--- Zipped Working UBUNTU/archive_name_latest/UBUNTU/fraud_detection_system/src/components/agents/tools/tools_registery.py
+++
@@ -1,7 +0,0 @@
-# Import native tools from specialized files
-from src.components.agents.tools.network_tools import query_graph_database
-
-# Categorized tool arrays
-behavioral_tools = []
-compliance_tools = []
-network_tools = [query_graph_database]
